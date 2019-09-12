ee53b0775981c8cd5e35b4601a38c2e05b98239b
Taylor Lacerte <taylormlacerte@gmail.com>
2019-09-11 22:33:46 -0500

    added toy and photo models

--- main_app/views.py
+++ main_app/views.py
@@ -1,8 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from .models import Dog
+from django.views.generic import ListView, DetailView
+import uuid
+import boto3
+from .models import Dog, Toy, Photo
+from .forms import FeedingForm
 
-# View functions
+S3_BASE_URL = 'https://s3-us-west-1.amazonaws.com/'
+BUCKET ="dogcollector-tml"
+
+class DogCreate(CreateView):
+  model = Dog
+  fields = ['name', 'breed', 'description', 'age']
+
+class DogUpdate(UpdateView):
+  model = Dog
+  fields = ['breed', 'description', 'age']
+
+class DogDelete(DeleteView):
+  model = Dog
+  success_url = '/dogs/'
 
 def home(request):
   return render(request, 'home.html')
@@ -16,16 +33,70 @@ def dogs_index(request):
 
 def dogs_detail(request, dog_id):
   dog = Dog.objects.get(id=dog_id)
-  return render(request, 'dogs/detail.html', { 'dog': dog })
+  toys_dog_doesnt_have = Toy.objects.exclude(id__in = dog.toys.all().values_list('id'))
+  # Instantiate FeedingForm to be rendered in the template
+  feeding_form = FeedingForm()
+  return render(request, 'dogs/detail.html', {
+    'dog': dog, 'feeding_form': feeding_form,
+    # Add the toys to be displayed
+    'toys': toys_dog_doesnt_have
+  })
 
-class DogCreate(CreateView):
-  model = Dog
+def add_feeding(request, dog_id):
+	# create the ModelForm using the data in request.POST
+  form = FeedingForm(request.POST)
+  # validate the form
+  if form.is_valid():
+    # don't save the form to the db until it
+    new_feeding = form.save(commit=False)
+    new_feeding.dog_id = dog_id
+    new_feeding.save()
+  return redirect('detail', dog_id=dog_id)
+
+def assoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.add(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+def unassoc_toy(request, dog_id, toy_id):
+  Dog.objects.get(id=dog_id).toys.remove(toy_id)
+  return redirect('detail', dog_id=dog_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
   fields = '__all__'
 
-class DogUpdate(UpdateView):
-  model = Dog
-  fields = ['breed', 'description', 'age']
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
 
-class DogDelete(DeleteView):
-  model = Dog
-  success_url = '/dogs/'
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys/'
+  
+def add_photo(request):
+  return 
+
+def add_photo(request, dog_id):
+    # photo-file will be the "name" attribute on the <input type="file">
+    photo_file = request.FILES.get('photo-file', None)
+    if photo_file:
+        s3 = boto3.client('s3')
+        # need a unique "key" for S3 / needs image file extension too
+        key = uuid.uuid4().hex[:6] + photo_file.name[photo_file.name.rfind('.'):]
+        # just in case something goes wrong
+        try:
+            s3.upload_fileobj(photo_file, BUCKET, key)
+            # build the full url string
+            url = f"{S3_BASE_URL}{BUCKET}/{key}"
+            photo = Photo(url=url, dog_id=dog_id)
+            photo.save()
+        except:
+            print('An error occurred uploading file to S3')
+    return redirect('detail', dog_id=dog_id)
+  
